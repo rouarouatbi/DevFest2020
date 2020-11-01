19ccc23d208768eb56f13361de7e5f480688126c
Hrabi <Hrabbi.ahmed88@gmail.com>
2020-11-01 11:16:53 +0100

    App demo

--- Client/src/app/_services/volunteer.service.ts
+++ Client/src/app/_services/volunteer.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from "@angular/common/http";
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { environment } from "src/environments/environment";
+
 @Injectable({
   providedIn: 'root'
 })
 export class VolunteerService {
 
-  constructor() { }
+  private _url = environment.api_url ;
+  constructor(private _http: HttpClient) { }
+
+  askHelp(data){
+    const headers = new HttpHeaders()
+    .set('Authorization', 'my-auth-token')
+    .set('Content-Type', 'application/json')
+    return this._http.post(this._url + '/help',data , {
+      headers: headers
+    });
+  }
+  
+  getBlooods(){
+    return this._http.get(this._url+ '/help');
+  }
+
+  
+  getHospitalNeed(){
+    return this._http.get(this._url+ '/blood');
+  }
 }
